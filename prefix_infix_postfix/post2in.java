c289c2981db869f569f131c726c58b30edee3725
ibukye <ramnjn0112mk@gmail.com>
2025-09-26 17:47:32 +0800

    improved for brackets

--- prefix_infix_postfix/post2in.java
+++ prefix_infix_postfix/post2in.java
@@ -15,25 +15,45 @@ public class post2in {
             }
             // Operator
             else {
-                String b = st.pop();
-                String a = st.pop();
-                StringBuilder newEquation = new StringBuilder();
-                newEquation.append("(");
-                newEquation.append(a);
-                newEquation.append(c);
-                newEquation.append(b);
-                newEquation.append(")");
-                st.push(newEquation.toString());
+                // if +, - then need ()
+                if (c=='+' || c=='-') {
+                    String b = st.pop();
+                    String a = st.pop();
+                    StringBuilder newEquation = new StringBuilder();
+                    newEquation.append("(");
+                    newEquation.append(a);
+                    newEquation.append(c);
+                    newEquation.append(b);
+                    newEquation.append(")");
+                    st.push(newEquation.toString());
+                } else {
+                    String b = st.pop();
+                    String a = st.pop();
+                    StringBuilder newEquation = new StringBuilder();
+                    newEquation.append(a);
+                    newEquation.append(c);
+                    newEquation.append(b);
+                    st.push(newEquation.toString());
+                }
+                
             }
             i++;
         }
         return st.peek().toString();
     }
 
+    public static String removeBrackets(String s) {
+        if (s.startsWith("(") && s.endsWith(")")) {
+            return s.substring(1, s.length()-1);
+        }
+        return s;
+    }
+
 
     public static void main(String[] args) {
         String postfix = "AB-DE+F*/";
         String infix = postfix2infix(postfix);
-        System.out.println(infix);
+        //System.out.println(infix);
+        System.out.println(removeBrackets(infix));
     }
 }
